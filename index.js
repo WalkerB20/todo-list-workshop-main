09b961d3200f4f1929a07b4dd14da7a11ef24bd8
WalkerB20 <bradenwalk24@gmail.com>
2023-12-12 19:44:42 -0500

    complete build

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function addToList() {
+let inputValue = document.getElementById("itemInput").value;
+console.log(inputValue);
+if (inputValue.trim() !== "") {
+    let ul = document.getElementById("list");
+    let li = document.createElement("li");
+    li.textContent = inputValue;
+
+
+li.addEventListener('click', function() {
+    this.remove();
+});
+
+ul.appendChild(li);
+document.getElementById("itemInput").value = "";
+}
+}
+
+let buttonClick = document.getElementById("addButton")
+buttonClick.addEventListener('click', addToList)
